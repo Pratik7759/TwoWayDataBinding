6cab6af04556824e1d66c0ec7f8297707eac2278
Pratik <pratik682019@gmail.com>
2023-02-21 11:14:56 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Demo';
+
+  name = 'pratik';
+  email = 'pratik@gmail.com';
+  contact = 2333;
+  password = '';
+}
